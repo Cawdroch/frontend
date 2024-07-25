001d0720afb797fedaf8e42ad49c175a6bb4a79d
Chris Cawdron <77812106+Cawdroch@users.noreply.github.com>
2024-07-25 22:53:30 +0100

    Created Common Header

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Header from "./components/Header";
 
 // App function //
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <Router>
       <div className="container">
+        <Header />
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
