3907ec23308471068190cea9aad9b0bfe1152ce9
Chris Cawdron <77812106+Cawdroch@users.noreply.github.com>
2024-08-02 19:35:26 +0100

    Completed task app and wrote spinner test

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,9 @@
 // Import modules
 
-import React from "react";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import TaskForm from "./TaskForm";
 
 // Create dashboard
 
@@ -19,8 +19,14 @@ const Dashboard = () => {
     <>
       <section className="heading">
         <h1>Welcome {user && user.name}</h1>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <button className="btn" onClick={() => navigate("/alltasks")}>
+            Check Tasks
+          </button>
+        </div>
         <p>Tasks Dashboard</p>
       </section>
+      <TaskForm />
     </>
   );
 };
