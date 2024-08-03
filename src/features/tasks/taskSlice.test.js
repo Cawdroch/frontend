c282f98517956b20776869f3b19148f05af06e3f
Chris Cawdron <77812106+Cawdroch@users.noreply.github.com>
2024-08-03 10:45:24 +0100

    Creating Tests

---
+++ src/features/tasks/taskSlice.test.js
@@ -0,0 +1,33 @@
+import { configureMockStore } from "redux-mock-store";
+import thunk from "redux-thunk";
+import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
+import { getTasks } from "./taskSlice";
+import taskService from "./taskService";
+
+const mockStore = configureMockStore([thunk]);
+const mock = new MockAdapter(axios);
+
+describe("taskSlice", () => {
+  let store;
+  beforeEach(() => {
+    store = mockStore({
+      task: {
+        tasks: [],
+        isError: false,
+        isSuccess: false,
+        isLoading: false,
+        message: "",
+      },
+      auth: {
+        user: { token: "mock_token" },
+      },
+    });
+  });
+  afterEach(() => {
+    mock.reset();
+    store.clearActions();
+  });
+
+  test('calls the task service to get tasks, async () =>{')
+});
